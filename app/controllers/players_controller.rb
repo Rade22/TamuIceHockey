88b7b85eb24259bcb1e569f740a509b7386c9f7c
Samuel Erickson <60996317+serickson017@users.noreply.github.com>
2021-02-14 22:30:05 -0600

    Update players_controller.rb

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,52 @@
+<<<<<<< Updated upstream
 class PlayersController < ApplicationController
   def index
   end
+=======
+module::Api
+  class PlayersController < ApplicationController
+    def index
+      @players = Player.all
+      render json: @players
+    end
+
+    def show
+      @players = Player.find(params[:id])
+      render json: @players
+    end
+
+    def create
+      @players = Player.new(players_params)
+
+      if @players.save
+        render json: @players, status: :created
+      else
+        render json: @players.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @players = Player.find(params[:id])
+      if @players.update(players_params)
+        render json: @players
+      else
+        render json: @players.errors, status: :unprocessable_entity
+      end
+    end
+    
+    def destroy
+      @players = Player.find(params[:id])
+      @players.destroyend
+    end
+
+      #####################################
+    private
+
+    def players_params
+      params
+        .require(:player)
+    end
+
+
+>>>>>>> Stashed changes
 end

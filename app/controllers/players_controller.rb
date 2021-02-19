1ed293f80027cdeea977ce7f70dbd76fae894819
Matthew Bunker <69060225+MatthewBunker@users.noreply.github.com>
2021-02-18 22:16:21 -0600

    new player entry
    Need to fix null entry for new player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,35 @@
 class PlayersController < ApplicationController
   def index
-    @players = Player.all
+    @player = Player.all
+  end
+
+  def show
+
+  end
+
+  def new
+      @player = Player.new
+  end
+
+  def create
+      @player = Player.new(players_params)  
+      if @player.save
+          redirect_to root_path, notice: "Player saved"
+      else
+          render :new
+      end
+  end
+  
+  def delete
+
+  end
+
+  def edit
+      
+  end
+   
+  def players_params
+      params.require(:player).permit(:first_name, :last_name, :position, :number)
   end
   
 end

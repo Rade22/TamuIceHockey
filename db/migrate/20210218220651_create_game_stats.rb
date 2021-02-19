0bd1a9913976c2e266261da3c380b78fffe7d051
Rylan <rwade_22@tamu.edu>
2021-02-18 19:39:47 -0600

    string to text in game_stats

--- db/migrate/20210218220651_create_game_stats.rb
+++ db/migrate/20210218220651_create_game_stats.rb
@@ -1,9 +1,9 @@
 class CreateGameStats < ActiveRecord::Migration[6.1]
   def change
     create_table :game_stats do |t|
-      t.string :opposing_team
+      t.text :opposing_team
       t.date :date
-      t.string :location
+      t.text :location
       t.integer :home_score
       t.integer :opposing_score
 

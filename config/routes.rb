32d9820af71d957e2aa1fab4fbc51c09bd24b3c4
Joshua Brent <zizzazzuz@tamu.edu>
2021-02-23 23:12:44 -0600

    Created controller for participations w/ crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'participations/new'
+  get 'participations/create'
+  get 'participations/update'
+  get 'participations/edit'
+  get 'participations/destroy'
+  get 'participations/index'
+  get 'participations/show'
   root 'players#index'
 
   resources :games do
@@ -13,10 +20,19 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :participations do
+    member do
+      get :delete
+    end
+  end
+
+
+
+
   get ':controller(/:action(/:id))'
 
   resources :players
   resources :games
-
+  resources :participations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

8b72843a124e9685deb6039f1d6a6bb34c963cc6
hnathehe2 <hoangnghiaanh04@tamu.edu>
2021-02-28 03:49:57 -0600

    I'm lost

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -62,3 +63,73 @@ ActiveRecord::Schema.define(version: 2021_02_23_062608) do
   end
 
 end
+=======
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_12_002213) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "against_team"
+    t.datetime "date"
+    t.string "location"
+    t.integer "score"
+    t.integer "opposing_score"
+    t.integer "game_stats_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "logins", force: :cascade do |t|
+    t.string "username"
+    t.string "hashed_password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "participations", force: :cascade do |t|
+    t.integer "player_id"
+    t.integer "game_id"
+    t.integer "player_stats_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "player_stats", force: :cascade do |t|
+    t.integer "games_played"
+    t.integer "wins"
+    t.integer "losses"
+    t.integer "overtime_shootout_losses"
+    t.integer "goals_against"
+    t.integer "time_on_ice"
+    t.integer "goals"
+    t.integer "assists"
+    t.integer "penalty_minutes"
+    t.integer "powerplay_goals"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "position"
+    t.integer "number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
+>>>>>>> added player_stat model

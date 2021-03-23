2a1624e3b220a30b69b0809578796c219560c5a1
Rylan <rwade_22@tamu.edu>
2021-03-22 22:40:07 -0500

    game spec file updates

--- spec/features/game_create_spec.rb
+++ spec/features/game_create_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe 'Creating a game', type: :feature do
 	scenario 'valid inputs' do
 		visit new_game_path
 		fill_in 'game_against_team', with: 'Test3'
-		fill_in 'game_date', with: '2021-02-24 22:00:00 UTC'
-		fill_in 'game_ring_name', with: 'unknown'
+		fill_in 'game_date', with: '2021-03-22'
+		fill_in 'game_time', with: '2:30 AM'
+		fill_in 'game_ring_name', with: 'Reed'
+		fill_in 'game_city', with: 'College Station'
+		fill_in 'game_state', with: 'TX'
 		click_on 'Create Game'
 		visit games_path
 		expect(page).to have_content('Test3')

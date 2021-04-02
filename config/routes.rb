4941465d45fc437820192f46e9ed6d9b58428dcb
hnathehe2 <hoangnghiaanh04@tamu.edu>
2021-04-02 13:31:41 -0500

    addding resource :analytics

--- config/routes.rb
+++ config/routes.rb
@@ -44,5 +44,6 @@ Rails.application.routes.draw do
   resources :players
   resources :games
   resources :participations
+  resources :analytics
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

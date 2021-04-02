6863a81b08dfaac174a1d45f19added4c1e48758
Matthew Bunker <69060225+MatthewBunker@users.noreply.github.com>
2021-04-01 21:40:47 -0500

    changed add game, and delete game.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,8 +7,6 @@ class GamesController < ApplicationController
     start_date = params.fetch(:start_date, Date.today).to_date
     @games = Game.where(date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
     @game = Game.all
-    
-    # @game_powerplayAttemps = Game.where(params[:powerplay_attemps])
   end
 
   def show

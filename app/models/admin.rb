50e57f7ed90529c0881efc43e43f9054bf0bcd9d
Joshua Brent <zizzazzuz@tamu.edu>
2021-04-05 16:24:58 -0500

    Removed new admin creation on login

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,6 +5,7 @@ class Admin < ApplicationRecord
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
     return nil unless email =~ /@google.com || @tamu.edu\z/
-    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
+    # puts email
+    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_by(email: email)
   end
 end

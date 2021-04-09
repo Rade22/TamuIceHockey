f5b391557b0572b02032307618673f8f8b43db89
hnathehe2 <hoangnghiaanh04@tamu.edu>
2021-04-08 19:24:49 -0500

    update graph with adjusted x title

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,6 +1,29 @@
 class AnalyticsController < ApplicationController
   def index
+    #for the graph score per game
+    @goal_score_season = []
+    @temp_goal_score_season = Game.order(:date).group(:date).group(:against_team).sum(:goals_for)
+    @temp_goal_score_season.each do |temp|
+      raw_value = temp[0]
+      raw_value[0] = raw_value[0].strftime("%Y %m %d")
+      @goal_score_season.append([raw_value,temp[1]])
+    end
+    @goal_against_season = []
+    @temp_goal_against_season = Game.order(:date).group(:date).group(:against_team).sum(:goals_against)
+    @temp_goal_against_season.each do |temp|
+      raw_value = temp[0]
+      raw_value[0] = raw_value[0].strftime("%Y %m %d")
+      @goal_against_season.append([raw_value,temp[1]])
+    end
+    #for the graph score per game
+
     @game_graph = Game.order(:date)
+    
     @participation_graph = Participation.order(:player_id)
+    
+    @win_game = 4
+    @lose_game = 1
+    @over_game = 2
+
   end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,6 +12,7 @@ class PlayersController < ApplicationController
 
   def show
     @player = Player.find(params[:id])
+    @player_perform = Participation.group(:player_id)
   end
 
   def new

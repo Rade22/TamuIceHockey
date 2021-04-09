02689a14a81254e8dc51b862b4a0461973a03655
hnathehe2 <hoangnghiaanh04@tamu.edu>
2021-04-08 22:13:11 -0500

    better graph part 2

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,5 +1,6 @@
 class AnalyticsController < ApplicationController
   def index
+
     #for the graph score per game
     @goal_score_season = []
     @temp_goal_score_season = Game.order(:date).group(:date).group(:against_team).sum(:goals_for)
@@ -17,13 +18,34 @@ class AnalyticsController < ApplicationController
     end
     #for the graph score per game
 
-    @game_graph = Game.order(:date)
+    @game_order = Game.order(:date)
     
     @participation_graph = Participation.order(:player_id)
     
-    @win_game = 4
-    @lose_game = 1
-    @over_game = 2
 
+    #for the graph %win lose pie chart
+    @win_game = 0
+    @lose_game = 0
+    @over_game = 0
+    @game_order.each do |game|
+      if game.goals_for.nil? == false and game.goals_against.nil? == false
+        if game.is_overtime
+          @over_game += 1
+        elsif game.goals_for > game.goals_against
+          @win_game += 1
+        else
+          @lose_game += 1
+        end
+      end
+    end
+    #for the graph %win lose pie chart
+
+    @total_goal = @game_order.sum(:goals_for)
+    @powerplay_goal = @game_order.sum(:PowerPlayGoals)
+    @normal_goal = @total_goal - @powerplay_goal
+    def trythis
+      pie_chart ({"Powerplay goals" => @powerplay_goal, "Other goals" => @normal_goal})
+    end
+    
   end
 end
--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,2 +1,5 @@
 module AnalyticsHelper
+    def trythis
+        pie_chart ({"Powerplay goals" => @powerplay_goal, "Other goals" => @normal_goal})
+      end
 end

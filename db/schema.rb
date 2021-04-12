14e8c9d84264f6ad8abc8fa010e716f80699d839
hnathehe2 <hoangnghiaanh04@tamu.edu>
2021-04-12 00:29:45 -0500

    change powerplay_goals name to conventional name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_024653) do
+ActiveRecord::Schema.define(version: 2021_04_12_052547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_024653) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "scrimmage"
-    t.integer "PowerPlayGoals"
+    t.integer "powerplay_goals"
   end
 
   create_table "participations", force: :cascade do |t|

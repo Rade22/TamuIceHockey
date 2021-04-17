cd6ce5164befb484e0587ecb105f7d7d412e5cc0
Matthew Bunker <69060225+MatthewBunker@users.noreply.github.com>
2021-04-17 18:31:23 -0500

    fixed player participation page
    -Put name at top of page and removed from each card.
    - Restructured participation tile header

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,7 +3,11 @@
 # helper for games module
 module GamesHelper
   def game_identity(game)
-    "vs #{game.against_team} on #{game.date.strftime('%Y-%m-%d')}"
+    "#{game.against_team}"
+  end
+
+  def game_date(game)
+    "(#{game.date.strftime('%m-%d-%y')})"
   end
 
   def us_states

0aff57dbff68161e961cbaf5230b28a45dca3177
Rylan <rwade_22@tamu.edu>
2021-04-19 18:12:22 -0500

    fixed flash messages and working for participations

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -34,7 +34,8 @@ class ParticipationsController < ApplicationController
   def create
     @participation = Participation.new(participations_params)
     if @participation.save
-      redirect_to playerParticipations_participation_path(Player.find(@participation.player_id)), notice: 'Participation saved'
+	  flash[:success] = 'Statistics Saved!'
+      redirect_to playerParticipations_participation_path(Player.find(@participation.player_id))
     else
       render :new
     end
@@ -49,7 +50,7 @@ class ParticipationsController < ApplicationController
   def destroy
     @participation = Participation.find(params[:id])
     @participation.destroy
-    flash.notice = 'Delete Participation Successfully'
+    flash[:success] = 'Statistics Deleted Successfully'
     redirect_to players_path
   end
 
@@ -62,7 +63,7 @@ class ParticipationsController < ApplicationController
   def update
     @participation = Participation.find(params[:id])
     if @participation.update(participations_params)
-      flash[:update] = 'Participation has been successfully updated!'
+      flash[:success] = 'Statitistics have been successfully updated!'
       redirect_to playerParticipations_participation_path(Player.find(@participation.player_id))
     else
       render 'edit'
--- app/helpers/participations_helper.rb
+++ app/helpers/participations_helper.rb
@@ -2,4 +2,16 @@
 
 # helper for participation module
 module ParticipationsHelper
+
+  def flash_class(level)
+  bootstrap_alert_class = {
+    "success" => "alert-success",
+    "error" => "alert-danger",
+    "notice" => "alert-info",
+    "alert" => "alert-danger",
+    "warn" => "alert-warning"
+  }
+  bootstrap_alert_class[level]
+end
+
 end

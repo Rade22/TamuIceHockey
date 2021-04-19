1a7adc6403e00f6fce4fb68cd81a4b39cd5021bc
hnathehe2 <hoangnghiaanh04@tamu.edu>
2021-04-19 18:53:47 -0500

    Update player pages, fixing minor error in analytics

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -13,7 +13,10 @@ module PlayersHelper
       @print_out = participate[0]
       @print_out_2 = participate[1]
       @current_game = @game_info.where(id: participate[0]).first
-      @player_goals.append([@current_game.against_team, participate[1]])
+      team_name = @current_game.against_team
+      game_date = @current_game.date
+      temp = team_name + " " + game_date.strftime('%m/%d/%Y')
+      @player_goals.append([temp, participate[1]])
     end
     
     line_chart [{name: "Goals", data: @player_goals}],
@@ -24,7 +27,8 @@ module PlayersHelper
         crosshair: true
       },
       yAxis: {
-        crosshair: true
+        crosshair: true,
+        allowDecimals: false
       }
     }
   end
@@ -36,8 +40,10 @@ module PlayersHelper
       @print_out = participate[0]
       @current_game = @game_info.where(id: participate[0]).first
       team_name = @current_game.against_team
-      @player_assists.append([@current_game.against_team, participate[1]])
-    end 
+      game_date = @current_game.date
+      temp = team_name + " " + game_date.strftime('%m/%d/%Y')
+      @player_assists.append([temp, participate[1]])
+    end
 
     line_chart [{name: "Goal assists", data: @player_assists}],
     colors: ["#800000"], 
@@ -47,10 +53,33 @@ module PlayersHelper
         crosshair: true
       },
       yAxis: {
-        crosshair: true
+        crosshair: true,
+        allowDecimals: false
       }
     }
-  end 
+  end
+
+  def player_score_per_game
+    @player_score = []
+    for i in 0..@player_assists.size()-1
+      score = @player_assists[i][1] + @player_goals[i][1]
+      @player_score.append([@player_assists[i][0],score])
+    end
+
+    line_chart [{name: "Player score by game", data: @player_score}],
+    colors: ["#800000"], 
+    library: {
+      title: {text: "Score by game"},
+      xAxis: {
+        crosshair: true
+      },
+      yAxis: {
+        crosshair: true,
+        allowDecimals: false
+      }
+    }
+    
+  end
 
   def penalty_minutes_skater
     @penalty_skater_raw = @player_perform.group(:game_id).sum(:penalty_minutes_skater)
@@ -59,14 +88,17 @@ module PlayersHelper
       @print_out = participate[0]
       @current_game = @game_info.where(id: participate[0]).first
       team_name = @current_game.against_team
-      @penalty_skater.append([@current_game.against_team, participate[1]])
+      game_date = @current_game.date
+      temp = team_name + " " + game_date.strftime('%m/%d/%Y')
+      @penalty_skater.append([temp, participate[1]])
     end
     line_chart [{name: "Penalty time", data: @penalty_skater}], 
     colors: ["#800000"],
     library: {
       title: {text: 'Penalty minutes per games'},
       yAxis: { 
-        crosshair: true
+        crosshair: true,
+        allowDecimals: false
       },
       xAxis: {
         crosshair: true
@@ -81,7 +113,10 @@ module PlayersHelper
       @print_out = participate[0]
       @print_out_2 = participate[1]
       @current_game = @game_info.where(id: participate[0]).first
-      @shots_against.append([@current_game.against_team, participate[1]])
+      team_name = @current_game.against_team
+      game_date = @current_game.date
+      temp = team_name + " " + game_date.strftime('%m/%d/%Y')
+      @shots_against.append([temp, participate[1]])
     end
 
     @saves_raw = @player_perform.group(:game_id).sum(:saves_goalie)
@@ -90,7 +125,10 @@ module PlayersHelper
       @print_out = participate[0]
       @print_out_2 = participate[1]
       @current_game = @game_info.where(id: participate[0]).first
-      @saves.append([@current_game.against_team, participate[1]])
+      team_name = @current_game.against_team
+      game_date = @current_game.date
+      temp = team_name + " " + game_date.strftime('%m/%d/%Y')
+      @saves.append([temp, participate[1]])
     end
     
     line_chart [{name: "Shots against", data: @shots_against},
@@ -102,7 +140,8 @@ module PlayersHelper
         crosshair: true
       },
       yAxis: {
-        crosshair: true
+        crosshair: true,
+        allowDecimals: false
       }
     }
   end
